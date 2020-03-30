41cc1789e09ec39b1570ed2d047497c92ed739ae
Adrian Alva <antrip_147@live.com>
2020-03-29 19:04:30 -0500

    Awa de UwU

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {NavLink}  from 'react-router-dom'
-import Awebo from'./../assets/coronavirus192.png'
-
+import Awebo from './../images/coronavirus192.png'
+import twitter from './../images/twitter.svg';
+import youtube from './../images/youtube.svg';
+import './../assets/styles/NavBarStyle.css'
 
 const Navbar = () => {
   return (
       <header>
 
         <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light">
-          <img className="Awebo" alt="" />
+          <img className = "Awebo" src = {Awebo} alt="" />
           <NavLink className="navbar-brand" to="/">Covid19</NavLink>
           
           <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,6 +28,8 @@ const Navbar = () => {
                 <NavLink className="nav-link" to="/DatosRegionesPeru">DatosRegionesPeru</NavLink>
               </li>
             </ul>
+            <img className="youtube" src={youtube} alt="" />
+            <img className="twitter" src={twitter} alt="" />
           </div>
         </nav>
       
