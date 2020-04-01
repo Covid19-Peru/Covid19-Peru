21e54d8edb14cbb2dbf9dd50c058716ff1dc2be4
Adrian Alva <antrip_147@live.com>
2020-03-31 21:17:19 -0500

    Relleno de la sección ¿Quien lo Hizo?

--- src/components/informacion/AcercaDeLosDesarrolladores.js
+++ src/components/informacion/AcercaDeLosDesarrolladores.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import '../../assets/styles/css/AcercaDeLosDesarrolladores.css'
+import Foto1 from  '../../assets/img/Un_wapo.jpg'
+import Foto2 from '../../assets/img/Otro_wapo.jpg'
 
 export default function AcercaDeLosDesarrolladores() {
     return (
-        <div>
-            <p>mostramos el nombre de los dessarrolladores con sus fotos y sus correos electronicos</p>
+        <div className="container marketing">
+            <div className="row">
+                <div className="col-lg-4">
+                    <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={Foto1} preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"></img>
+                    <h2>Adrian Alva</h2>
+                    <p>Diseñador Front-End.</p> 
+                    <p>Comencé principalmente este proyecto para brindar información a las personas sobre este virus.
+                       Me gusta mucho la programación, así como el diseño de interfaces de Paginas Web.
+                    </p>
+                    <p><a className="btn btn-secondary" href="https://www.linkedin.com/in/adrian-rodrigo-alva-tiznado-0322a21a6/" role="button">Mas Detalles »</a></p>
+                </div>
+                <div className="col-lg-4">
+                <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={Foto2} preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"></img>
+                    <h2>José Cueva</h2>
+                    <p>Programador Back-End</p>
+                    <p>Comenzó, yo que sé que lo escriba él.</p>
+                    <p><a className="btn btn-secondary" href="https://www.linkedin.com/in/cuevacelis/" role="button">View details »</a></p>
+                </div>
+                <div className="col-lg-4">
+                <img className="bd-placeholder-img rounded-circle" width="140" height="140" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"></img>
+                    <h2>Anderson Perales</h2>
+                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                    <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+                </div>
+            </div>
+            <hr class="featurette-divider" />
         </div>
     )
 }
