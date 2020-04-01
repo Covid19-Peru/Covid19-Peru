83d294a8627b7b024292ae010d6c95fd4fb06eda
Adrian Alva <antrip_147@live.com>
2020-04-01 12:55:35 -0500

    Se agregó en el Footer un Mensaje

--- src/components/informacion/AcercaDeLosDesarrolladores.js
+++ src/components/informacion/AcercaDeLosDesarrolladores.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import '../../assets/styles/css/AcercaDeLosDesarrolladores.css'
 import Foto1 from  '../../assets/img/Un_wapo.jpg'
 import Foto2 from '../../assets/img/jose-cueva-celis.jpg'
 import Foto3 from '../../assets/img/ElPapiChurro.jpg'
+
+const Footer = lazy(() => import('./../Footer'));
 
 export default function AcercaDeLosDesarrolladores() {
     return (
@@ -11,29 +13,36 @@ export default function AcercaDeLosDesarrolladores() {
                 <div className="col-lg-4">
                     <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={Foto1} preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"></img>
                     <p> </p> 
-                    <h2>Adrian Alva</h2>
-                    <p>Diseñador Front-End.</p> 
+                    <h2>Adrian Alva Tiznado</h2>
+                    <p>Diseñador Front-End y Desarrollador De Software.</p> 
                     <p>Comencé principalmente este proyecto para brindar información a las personas sobre este virus.
-                       Me gusta mucho la programación, así como el diseño de interfaces de Paginas Web.
+                       Me caracterizo principalmente por ayudar a mi equipo en completar las metas propuestas. También he liderado proyectos de este tipo, así como, proyectos de Software Libre.
+                       Me gusta mucho la programación, así como, el diseño de interfaces de Páginas Web.
                     </p>
-                    <p><a className="btn btn-secondary" href="https://www.linkedin.com/in/adrian-rodrigo-alva-tiznado-0322a21a6/" role="button">Ver más detalles »</a></p>
+                    <p><a className="btn btn-secondary" href="https://www.linkedin.com/in/adrianalvatiznado/" role="button">Ver más detalles »</a></p>
                 </div>
                 <div className="col-lg-4">
                 <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={Foto2} preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"></img>
                     <p> </p> 
                     <h2>José Cueva Celis</h2>
                     <p>Programador Back-End, Front-End y Fundador.</p>
-                    <p>Comenzó, yo que sé que lo escriba él.</p>
+                    <p>Comencé este proyecto para brindar información a las personas. 
+                       Me caracterizo por liderar proyectos, compartir mis conocimientos, y ayudar a mi equipo para completar las metas propuestas. 
+                       Soy apasionado por el lenguaje nativo y Javasript.</p>
                     <p><a className="btn btn-secondary" href="https://www.linkedin.com/in/cuevacelis/" role="button">Ver más detalles »</a></p>
                 </div>
                 <div className="col-lg-4">
                 <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={Foto3} preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"></img>
                     <p> </p> 
                     <h2>Anderson Perales</h2>
-                    <p>Programador Front-End, base de datos en SQLServer y documentación de proyectos. Comencé principalmente este proyecto para brindar información a las personas sobre este virus llamado COVID-19.
-                    Me gusta mucho diseño de interfaces de Paginas Web.</p>
+                    <p>Programador Front-End, Base de Datos en SQLServer y Documentación de Proyectos.</p> 
+                    <p> </p> 
+                    <p>Comencé principalmente este proyecto para brindar información a las personas sobre este virus llamado COVID-19.
+                        Me gusta mucho diseño de interfaces de Paginas Web.</p>
                     <p><a className="btn btn-secondary" href="https://pe.linkedin.com/in/anderson-nolberto-perales-l%C3%B3pez-8655b5193?trk=people-guest_people_search-card" role="button">Ver más detalles »</a></p>
                 </div>
+
+                <Footer />
 
                 <div className="social">
                             <ul>
