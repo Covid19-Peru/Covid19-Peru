ca123a9f0e1758d8f1168a3762ead216dbad4110
Adrian Alva <antrip_147@live.com>
2020-04-11 19:37:01 -0500

    Cambios en el celular

--- src/components/informacion/ComoHicimosLaPagina.js
+++ src/components/informacion/ComoHicimosLaPagina.js
@@ -35,30 +35,30 @@ export default function ComoHicimosLaPagina() {
                     </div>
                     <div className="bg-light shadow-sm mx-auto foto-d" style={{width: "80%", height: "300px", borderRadius: "21px 21px 0 0"}}>
                         <div className="container" style={{height: "300px", borderRadius: "21px 21px 0 0"}}>
-                            <img className="bd-placeholder-img rounded-circle" width="100" height="100" src={Foto1} style={{float:"left", marginTop:"50px", marginLeft: "20px"}} focusable="false" role="img"></img>
-                            <div style={{margin:"0 auto"}}>
-                                <div className="message-content bg-dark" style={{ fontSize: "15px",width: "40%", borderRadius:"21px 21px 21px 0", marginLeft: "130px"}}>
-                                    <div className="contenedor">
-                                        <div className="bd-dark" style={{marginTop:"70px"}}>
+                            <img className="bd-placeholder-img rounded-circle" width="100" height="100" src={Foto1} style={{float:"left", marginTop:"50px", marginLeft: "6%"}} focusable="false" role="img"></img>
+                            <div>
+                                <div className="message-content bg-dark" style={{ fontSize: "15px",width: "40%", borderRadius:"21px 21px 21px 0", marginLeft: "130px", height:"auto", marginTop:"70px"}}>
+                                    
+                                        <div className="bd-dark">
                                             <div>Yo haré el Front-End.</div>
                                             <div className="mt-1">
                                                 <small className="opacity-65">8 mins ago</small>
                                             </div>
                                         </div>
-                                    </div>
+                                    
                                 </div>
                             </div>
                             <img className="bd-placeholder-img rounded-circle" width="100" height="100" src={Foto2} style={{float:"right", marginTop:"20px", marginRight: "20px"}} focusable="false" role="img"></img>
-                            <div style={{margin:"0 auto"}}>
-                                <div className="message-content bg-dark" style={{ fontSize: "15px",width: "30%", borderRadius:"21px 21px 0 21px", marginLeft: "40%"}}>
-                                    <div className="contenedor">
-                                        <div className="bd-dark" style={{marginTop:"45px"}}>
+                            <div>
+                                <div className="message-content bg-dark" style={{ fontSize: "15px",width: "30%", borderRadius:"21px 21px 0 21px", marginTop:"34px", height:"auto", width:"30%", float:"right", marginRight:"10px"}}>
+                                    
+                                        <div className="bd-dark">
                                             <div>oc.</div>
                                             <div className="mt-1">
                                                 <small className="opacity-65">right now.</small>
                                             </div>
                                         </div>
-                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -74,24 +74,24 @@ export default function ComoHicimosLaPagina() {
                     </div>
                     <div className="bg-dark shadow-sm mx-auto" style={{width: "80%", height: "300px", borderRadius:"21px 21px 0 0"}}>
                         <div className="container bg-dark" style={{height: "300px", borderRadius: "21px 21px 0 0"}}>
-                            <img className="bd-placeholder-img rounded-circle" width="100" height="100" src={Foto3} style={{float:"left", marginTop:"50px", marginLeft: "10%"}} focusable="false" role="img"></img>
+                            <img className="bd-placeholder-img rounded-circle" width="100" height="100" src={Foto3} style={{float:"left", marginTop:"40px", marginLeft: "6%"}} focusable="false" role="img"></img>
                             <div style={{margin:"0 auto"}}>
-                                <div className="message-content bg-light" style={{ fontSize: "15px",width: "40%", borderRadius:"21px 21px 21px 0", marginLeft: "135px"}}>
-                                    <div className="contenedor">
-                                        <div className="bd-light" style={{marginTop:"70px"}}>
-                                            <div>Herramientas que usamos.</div>
-                                            <div className="mt-1">
-                                                <small className="opacity-65">5 mins ago</small>
-                                            </div>
+                                <div className="message-content bg-light" style={{ fontSize: "15px", borderRadius:"21px 21px 21px 0", marginLeft: "130px", height:"auto", marginTop:"55px"}}>
+                                    
+                                    <div className="bd-light">
+                                        <div>Herramientas que usamos.</div>
+                                        <div className="mt-1">
+                                            <small className="opacity-65">5 mins ago</small>
                                         </div>
                                     </div>
+                                    
                                 </div>
                             </div>
-                            <div className="row" style={{marginTop:"60px", justifyContent:"center"}}>
-                                <div className="col-xs-6">
+                            <div className="row" style={{marginTop:"60px", justifyContent:"center", minWidth: "100%"}}>
+                                <div className="col-xs-6" style={{marginRight:"10px"}}>
                                     <img className="bd-placeholder-img" width="60" height="60" src={JavaScript} focusable="false" role="img"></img>
                                 </div> 
-                                <div className="col-xs-6">
+                                <div className="col-xs-6" style={{marginRight:"10px"}}>
                                     <img className="bd-placeholder-img" width="60" height="60" src={LogoReact} focusable="false" role="img"></img>
                                 </div>                           
                                 <div className="col-xs-6">
