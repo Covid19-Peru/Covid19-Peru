b9a50929f8fed5376cc4a325f099c13d6404d8c3
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-05-02 13:45:16 -0500

    cambios package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
-serviceWorker.unregister();
+//serviceWorker.unregister();
