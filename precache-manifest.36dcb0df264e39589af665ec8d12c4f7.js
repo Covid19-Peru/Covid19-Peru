58fcf38173201bf41f93e341c488c90bb7c171f9
Jose Cueva Celis <cuevacelis@hotmail.com>
2020-05-03 00:16:06 -0500

    Updates

---
+++ precache-manifest.36dcb0df264e39589af665ec8d12c4f7.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf5d937effda277ef51b3117e65e32d",
+    "url": "/Covid19-Peru/index.html"
+  },
+  {
+    "revision": "e30357f63864eb8f11c6",
+    "url": "/Covid19-Peru/static/css/10.9a96c5f8.chunk.css"
+  },
+  {
+    "revision": "112fd01be67233463737",
+    "url": "/Covid19-Peru/static/css/11.45ea575d.chunk.css"
+  },
+  {
+    "revision": "e4c818f6cfb9dd495018",
+    "url": "/Covid19-Peru/static/css/12.cf352dba.chunk.css"
+  },
+  {
+    "revision": "da57f4a6704a76b0fb27",
+    "url": "/Covid19-Peru/static/css/3.0074a4f9.chunk.css"
+  },
+  {
+    "revision": "7edd939b43a281221eda",
+    "url": "/Covid19-Peru/static/css/4.45ea575d.chunk.css"
+  },
+  {
+    "revision": "eebc07cd6db3899f039a",
+    "url": "/Covid19-Peru/static/css/5.732c2429.chunk.css"
+  },
+  {
+    "revision": "9829a5de0cb075a50f08",
+    "url": "/Covid19-Peru/static/css/8.4a62ee93.chunk.css"
+  },
+  {
+    "revision": "acdb2458936c04369586",
+    "url": "/Covid19-Peru/static/css/9.6c53eb7d.chunk.css"
+  },
+  {
+    "revision": "9356e8d046c6e7d8f3fb",
+    "url": "/Covid19-Peru/static/css/main.6b72a821.chunk.css"
+  },
+  {
+    "revision": "a780ffe414146cfbcc62",
+    "url": "/Covid19-Peru/static/js/index.0.9ebbee72.production.chunk.min.js"
+  },
+  {
+    "revision": "e30357f63864eb8f11c6",
+    "url": "/Covid19-Peru/static/js/index.10.cb94b640.production.chunk.min.js"
+  },
+  {
+    "revision": "112fd01be67233463737",
+    "url": "/Covid19-Peru/static/js/index.11.bd35f38f.production.chunk.min.js"
+  },
+  {
+    "revision": "e4c818f6cfb9dd495018",
+    "url": "/Covid19-Peru/static/js/index.12.422235da.production.chunk.min.js"
+  },
+  {
+    "revision": "a4bf6fdb8c0be83fa42e",
+    "url": "/Covid19-Peru/static/js/index.13.35539c32.production.chunk.min.js"
+  },
+  {
+    "revision": "da57f4a6704a76b0fb27",
+    "url": "/Covid19-Peru/static/js/index.3.38881e12.production.chunk.min.js"
+  },
+  {
+    "revision": "1cbc60dbad5345c0952e4bd6bd15d4b8",
+    "url": "/Covid19-Peru/static/js/index.3.38881e12.production.chunk.min.js.LICENSE.txt"
+  },
+  {
+    "revision": "7edd939b43a281221eda",
+    "url": "/Covid19-Peru/static/js/index.4.7e5d5891.production.chunk.min.js"
+  },
+  {
+    "revision": "eebc07cd6db3899f039a",
+    "url": "/Covid19-Peru/static/js/index.5.7a77031f.production.chunk.min.js"
+  },
+  {
+    "revision": "ad27662b6d5b0013c814",
+    "url": "/Covid19-Peru/static/js/index.6.11b949f3.production.chunk.min.js"
+  },
+  {
+    "revision": "513032af4d821e1a6b86",
+    "url": "/Covid19-Peru/static/js/index.7.ef442dbc.production.chunk.min.js"
+  },
+  {
+    "revision": "9829a5de0cb075a50f08",
+    "url": "/Covid19-Peru/static/js/index.8.e1b89978.production.chunk.min.js"
+  },
+  {
+    "revision": "acdb2458936c04369586",
+    "url": "/Covid19-Peru/static/js/index.9.266794da.production.chunk.min.js"
+  },
+  {
+    "revision": "8a1632b9af7fafce5f84",
+    "url": "/Covid19-Peru/static/js/index.939c6bac.production.min.js"
+  },
+  {
+    "revision": "9356e8d046c6e7d8f3fb",
+    "url": "/Covid19-Peru/static/js/index.main.e8cd8003.production.chunk.min.js"
+  },
+  {
+    "revision": "0b144332ef224c55b7a5d1a6c183ec46",
+    "url": "/Covid19-Peru/static/media/ElPapiChurro.0b144332.jpg"
+  },
+  {
+    "revision": "115563dc17f768eafed7ef0c6253fa24",
+    "url": "/Covid19-Peru/static/media/Un_wapo.115563dc.jpg"
+  },
+  {
+    "revision": "9e609414a58194f824ad8fef740ed12a",
+    "url": "/Covid19-Peru/static/media/conversacion.9e609414.svg"
+  },
+  {
+    "revision": "d7afa43e8273de4bcdf849423dceb20b",
+    "url": "/Covid19-Peru/static/media/covid_19.d7afa43e.svg"
+  },
+  {
+    "revision": "c64430b2948cadf3e67a1a01b8fb1f78",
+    "url": "/Covid19-Peru/static/media/css-3.c64430b2.svg"
+  },
+  {
+    "revision": "231a5fc81a7a78d0308dccd5cabecc6e",
+    "url": "/Covid19-Peru/static/media/herramienta.231a5fc8.svg"
+  },
+  {
+    "revision": "73cd6e837bf0a9fbf55d6baa5f2b7ab2",
+    "url": "/Covid19-Peru/static/media/icomoon.73cd6e83.woff"
+  },
+  {
+    "revision": "79b0c7dcdf84a0b1c602b266120f5635",
+    "url": "/Covid19-Peru/static/media/icomoon.79b0c7dc.eot"
+  },
+  {
+    "revision": "ab73b031a1039598636ba8f7e5ea37c2",
+    "url": "/Covid19-Peru/static/media/icomoon.ab73b031.svg"
+  },
+  {
+    "revision": "e462b23a3ebd1e11443e1077f68f26d7",
+    "url": "/Covid19-Peru/static/media/icomoon.e462b23a.ttf"
+  },
+  {
+    "revision": "037f2a0625c78965d7d3a36547baca80",
+    "url": "/Covid19-Peru/static/media/javascript.037f2a06.svg"
+  },
+  {
+    "revision": "ea281c140abab3c8337e82dc1729d93e",
+    "url": "/Covid19-Peru/static/media/jose-cueva-celis.ea281c14.jpg"
+  },
+  {
+    "revision": "83946a33ce2612175fca81972fffd4b0",
+    "url": "/Covid19-Peru/static/media/lavar las manos.83946a33.jpg"
+  },
+  {
+    "revision": "663c0b554e32200cc45f748677c3788e",
+    "url": "/Covid19-Peru/static/media/llamar.663c0b55.jpg"
+  },
+  {
+    "revision": "a39b3f198283271a11fbbc026de65169",
+    "url": "/Covid19-Peru/static/media/pañuelo.a39b3f19.jpg"
+  },
+  {
+    "revision": "5257bedaec6beae772b699f1e3fa6e74",
+    "url": "/Covid19-Peru/static/media/reaccionar.5257beda.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid19-Peru/precache-manifest.cf8568c5f022896b8e67fa8bab0c1fb0.js"
+  "/Covid19-Peru/precache-manifest.36dcb0df264e39589af665ec8d12c4f7.js"
 );
 
 self.addEventListener('message', (event) => {
